07dc43c7998894b5feeb7da96b8312a753b7af27
g1derekl <derek.li@mail.utoronto.ca>
2014-02-27 09:49:08 -0500

    Implemented crawling of players array

--- crawler.js
+++ crawler.js
@@ -1,14 +1,20 @@
 var http = require("http")
 , cheerio = require("cheerio")
-, src = "http://www.fleaflicker.com/nba/league?leagueId=11345";
+, src = ["http://www.fleaflicker.com/nba/team?leagueId=11345&teamId=73630"
+       , "http://www.fleaflicker.com/nba/team?leagueId=11345&teamId=73516"
+       , "http://www.fleaflicker.com/nba/team?leagueId=11345&teamId=73667"
+       , "http://www.fleaflicker.com/nba/team?leagueId=11345&teamId=73676"
+       , "http://www.fleaflicker.com/nba/team?leagueId=11345&teamId=73660"
+       , "http://www.fleaflicker.com/nba/team?leagueId=11345&teamId=72898"]; // For test purposes only; will load from database when ready
 
+/* Download HTML from web page at URL */
 var download = function(url, callback) {
-  http.get(url, function(res) {
+  http.get(url, function(response) {
     var data = "";
-    res.on("data", function (chunk) {
+    response.on("data", function (chunk) {
       data += chunk;
     });
-    res.on("end", function() {
+    response.on("end", function() {
       callback(data);
     });
   }).on("error", function() {
@@ -16,10 +22,7 @@ var download = function(url, callback) {
   });
 }
 
-var parsePlayers = function(league) {
-  
-}
-
+/* Given HTML data of a player, parse field goal percentages */
 var parseShooting = function(data) {
   var $ = cheerio.load(data);
   var stats = {};
@@ -28,14 +31,25 @@ var parseShooting = function(data) {
   stats["fga"] = parseInt($("#row_0_0_10 td").eq(16).text().split("-")[1]);
   stats["fg%"] = stats["fgm"] / stats["fga"];
   
-  console.log(JSON.stringify(stats));
   return stats;
 }
 
+/* Get field goal percentages from all players in league */
 var getData = function(callback) {
-  players = [];
-  download(src, function(league) {
+  var numRunningQueries = 0;
+  var shootingStats = [];
+  
+  src.forEach(function(player) {
+    numRunningQueries++;
     
+    download(player, function(data) {
+      shootingStats.push(parseShooting(data));
+      numRunningQueries--;
+      
+      if (numRunningQueries == 0) {
+       callback(shootingStats);
+      }
+    });
   });
 }
 
