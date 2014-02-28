6e33fe2674de6e91263d15569794a641fe2da9db
g1derekl <derek.li@mail.utoronto.ca>
2014-02-28 08:10:53 -0500

    Implemented tabulation of other stats

--- crawler.js
+++ crawler.js
@@ -1,7 +1,9 @@
 var http = require("http")
 , cheerio = require("cheerio")
+, fs = require("fs")
 , DAYS = 122
-, players = {"73516": {"league": 11345, "fgm": 0, "fga": 0}, "73630": {"league": 11345, "fgm": 0, "fga": 0}, "73493": {"league": 11346, "fgm": 0, "fga": 0}, "73618": {"league": 11346, "fgm": 0, "fga": 0}}; // For test purposes only; will load from database when ready
+, players = {"73516": {"league": 11345, "fgm": 0, "fga": 0}, "73630": {"league": 11345, "fgm": 0, "fga": 0}, "73493": {"league": 11346, "fgm": 0, "fga": 0}, "73618": {"league": 11346, "fgm": 0, "fga": 0}} // For test purposes only; will load from database when ready
+, leagues = [11345, 11346];
 
 /* Download HTML from web page at URL */
 var download = function(url, callback) {
@@ -25,19 +27,36 @@ var parseShooting = function(team, data) {
   players[team]["team"] = $("#top-bar .breadcrumb .active h1").text();
   players[team]["user"] = $(".user-name").text();
   
-  var fgmTotal = 0, fgaTotal = 0;
   for (var row=0; row < 10; row++) {
     var fg = $("#row_0_0_" + row + " td").eq(16).text().split("-");
     var fgm = parseInt(fg[0]);
     var fga = parseInt(fg[1]);
     if (fgm >= 0) {
       players[team]["fgm"] += fgm;
-      fgmTotal += fgm;
       players[team]["fga"] += fga;
-      fgaTotal += fga;
     }
   }
 };
+
+/* Take league page data and return stats for each player */
+var teamStats = function(data) {
+  var $ = cheerio.load(data);
+  
+  var numTeams = $(".cell-row").length;
+  console.log(numTeams);
+  
+  for (var row=0; row < 2; row++) {
+    var teamID = $("#row_0_0_" + row + " .league-name a").attr("href").substring(32, 37);
+    players[teamID]["ft%"] = $("#row_0_0_" + row + " td").eq(7).text();
+    players[teamID]["3pt"] = $("#row_0_0_" + row + " td").eq(10).text();
+    players[teamID]["reb"] = $("#row_0_0_" + row + " td").eq(13).text();
+    players[teamID]["stl"] = $("#row_0_0_" + row + " td").eq(16).text();
+    players[teamID]["blk"] = $("#row_0_0_" + row + " td").eq(19).text();
+    players[teamID]["ast"] = $("#row_0_0_" + row + " td").eq(22).text();
+    players[teamID]["tov"] = $("#row_0_0_" + row + " td").eq(25).text();
+    players[teamID]["pts"] = $("#row_0_0_" + row + " td").eq(28).text();
+  }
+}
 
 /* Get field goal percentages from all teams in league */
 var getFGData = function(callback) {
@@ -54,7 +73,7 @@ var getFGData = function(callback) {
         
         numRunningQueries--;
         if (numRunningQueries == 0) {
-          callback(players);
+          callback();
         }
       });
     }
@@ -62,7 +81,23 @@ var getFGData = function(callback) {
 };
 
 var compileStats = function(callback) {
+  var numRunningQueries = 0;
   
+  getFGData(function() {
+    for (var leagueID=0; leagueID < leagues.length; leagueID++) {
+      numRunningQueries++;
+      var url = "http://www.fleaflicker.com/nba/league?leagueId=" + leagues[leagueID];
+      
+      download(url, function(data) {
+        teamStats(data);
+        
+        numRunningQueries--;
+        if (numRunningQueries == 0) {
+          callback(players);
+        }
+      });
+    }
+  });
 };
 
-module.exports.getFGData = getFGData;
+module.exports.compileStats = compileStats;
