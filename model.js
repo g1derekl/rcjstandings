00c89de5fe7c656b13e452d54ff200d88e6812e9
g1derekl <derek.li@mail.utoronto.ca>
2014-03-08 16:43:34 -0500

    Added duplicate handling for DB

--- model.js
+++ model.js
@@ -30,12 +30,14 @@ var download = function(url, callback) {
 /* Build the data set of teams */
 var buildTeamList = function(callback) {
   var numRunningQueries = 0;
-  var prepareLeagues = "INSERT INTO leagues (league_id, league_name) VALUES (?, ?)";
-  var prepareTeams = "INSERT INTO teams (team_id, team_name, player_name, league_id) VALUES (?, ?, ?, ?)";
+  var prepareLeagues = "INSERT INTO leagues (league_id, league_name) VALUES (?, ?)"
+    + "ON DUPLICATE KEY UPDATE league_id = ?, league_name = ?";
+  var prepareTeams = "INSERT INTO teams (team_id, team_name, player_name, league_id) VALUES (?, ?, ?, ?)"
+    + "ON DUPLICATE KEY UPDATE team_name = ?, player_name = ?";
   // NOTE: important to update when duplicate key in case league and/or team changes names or is taken over by another user
   
   Object.keys(leagues).forEach(function(leagueID) {
-    var insertLeagues = [leagueID, leagues[leagueID]];
+    var insertLeagues = [leagueID, leagues[leagueID], leagueID, leagues[leagueID]];
     var preparedLeagues = mysql.format(prepareLeagues, insertLeagues);
     connection.query(preparedLeagues, function(error) {
       if (error) console.log(error);
@@ -53,7 +55,7 @@ var buildTeamList = function(callback) {
         var teamName = $("#row_0_0_" + row + " .league-name a").text();
         var playerName = $("#row_0_0_" + row + " .right a").text();
         
-        var insertTeams = [teamID, teamName, playerName, leagueID];
+        var insertTeams = [teamID, teamName, playerName, leagueID, teamName, playerName];
         var preparedTeams = mysql.format(prepareTeams, insertTeams);
         connection.query(preparedTeams, function(error) {
           if (error) console.log(error);
@@ -71,6 +73,11 @@ var buildTeamList = function(callback) {
       }
     });
   });
+};
+
+/* Take SQL query and send back results */
+var getData = function(callback) {
+  
 };
 
 module.exports.buildTeamList = buildTeamList;
